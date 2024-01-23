56f23890077c51d2928433194f2bcedee8aead84
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2024-01-23 21:24:10 +0800

    Consensus breaking changes

--- Q5/blog/x/blog/keeper/msg_server_create_post.go
+++ Q5/blog/x/blog/keeper/msg_server_create_post.go
@@ -4,21 +4,22 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"blog/x/blog/types"
 )
 
 func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Breaking Change: Change the key used to store the post in the underlying key-value store
+	storeKey := []byte("new_post_key/") + sdk.Uint64ToBigEndian(msg.Id).Bytes()
+
 	var post = types.Post{
 		Creator: msg.Creator,
 		Title:   msg.Title,
 		Body:    msg.Body,
 	}
-	id := k.AppendPost(
-		ctx,
-		post,
-	)
+	id := k.AppendPost(ctx, post, storeKey) // Update the AppendPost function to accept the store key
+
 	return &types.MsgCreatePostResponse{
 		Id: id,
 	}, nil
